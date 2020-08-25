1a01a2148d623ca105b66a2557523d395ae11916
lesstif <lesstif@gmail.com>
2020-08-25 14:51:22 +0900

    composer.lock 제거

--- database/factories/ProjectCategoryFactory.php
+++ database/factories/ProjectCategoryFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\ProjectCategory::class, function (Faker $faker) {
     return [
-        'name' => $faker->realText(20),
+        'name' => $faker->company,
         'desc' => $faker->realText,
         'created_at' => $faker->dateTimeBetween('-3 years', 'now'),
     ];
